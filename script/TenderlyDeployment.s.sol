5bc0ace446a290f799eb17ff4c5939904d1b97d5
Andrew Chiaramonte <andrewchiaramonte4104@gmail.com>
2024-10-14 14:05:23 -0400

    remove excess in deploy command

--- script/TenderlyDeployment.s.sol
+++ script/TenderlyDeployment.s.sol
@@ -31,7 +31,7 @@ contract TenderlyDeployment is Script {
  * > source .env
  *
  * 2. Deploy and verify our contract
- * > forge script script/TenderlyDeployment.s.sol:TenderlyDeployment --rpc-url $BASE_TENDERLY_RPC_URL --broadcast -vvvv --etherscan-api-key $ETHERSCAN_API_KEY --verify
+ * > forge script script/TenderlyDeployment.s.sol:TenderlyDeployment --rpc-url $BASE_TENDERLY_RPC_URL --broadcast -vvvv
  *
  * 3. Verify the contract
  * > forge verify-contract CONTRACT_ADDRESS src/MockClearinghouse.sol:MockClearinghouse --etherscan-api-key $TENDERLY_ACCESS_KEY --verifier-url $TENDERLY_VERIFIER_URL --watch
